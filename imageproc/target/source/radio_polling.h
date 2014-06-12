7d07538165ae7c78c327310660d736afd6ad377a
ETumang <emily.tumang@gmail.com>
2014-06-12 15:55:24 -0400

    Added radio bootloading macro.

--- imageproc/target/source/radio_polling.h
+++ imageproc/target/source/radio_polling.h
@@ -7,43 +7,43 @@
 #ifndef __RADIO_H
 #define __RADIO_H
 
-#if defined(__C1)
-	#define DEFAULT_DEST_ADDR        0x0101
+#if defined(__S1)
 	#define DEFAULT_SRC_ADDR                0x0011
 
-#elif defined(__C2)
-	#define DEFAULT_DEST_ADDR        		0x0101
+#elif defined(__S2)
 	#define DEFAULT_SRC_ADDR                0x0012
 	
-#elif defined(__C3)
-	#define DEFAULT_DEST_ADDR        		0x0101
+#elif defined(__S3)
 	#define DEFAULT_SRC_ADDR                0x0013
 	
-#elif defined(__C4)
-	#define DEFAULT_DEST_ADDR        		0x0101
+#elif defined(__S4)
 	#define DEFAULT_SRC_ADDR                0x0014
 
-#elif defined(__C5)
-	#define DEFAULT_DEST_ADDR        		0x0100
+#elif defined(__S5)
 	#define DEFAULT_SRC_ADDR                0x0015
 	
-#elif defined(__C6)
-	#define DEFAULT_DEST_ADDR        		0x0100
+#elif defined(__S6)
 	#define DEFAULT_SRC_ADDR                0x0016
 
-#elif defined(__C7)
-	#define DEFAULT_DEST_ADDR        		0x0100
+#elif defined(__S7)
 	#define DEFAULT_SRC_ADDR                0x0017
 
-#elif defined(__C8)
-	#define DEFAULT_DEST_ADDR        		0x0100
+#elif defined(__S8)
 	#define DEFAULT_SRC_ADDR                0x0018
 
 #else
-	#define DEFAULT_DEST_ADDR        		0x0100
 	#define DEFAULT_SRC_ADDR                0x0110
 
 #endif
+
+#if defined(__D0100)
+	#define DEFAULT_DEST_ADDR				0x0100
+
+#else
+	#define DEFAULT_DEST_ADDR				0x0101
+
+#endif
+
 void radioSetup(void);
 
 /*****************************************************************************

5d1226e1bd88b3f89ee41e8b4ba5547f20f778af
ETumang <emily.tumang@gmail.com>
2014-06-13 18:06:54 -0400

    Fun with memory.  Can now write to an address.

--- imageproc/target/source/main.c
+++ imageproc/target/source/main.c
@@ -12,30 +12,67 @@
 #include "utils.h"
 #include <xc.h>
 
-//unsigned int Ntwaddr = {DEFAULT_SRC_ADDR, DEFAULT_DEST_ADDR, DEFAULT_CHANNEL};
-//#define _EEDATA(N) __attribute__((space(eedata), aligned(4)), 
+#define SRC_ADDR_LOC        0x400
+
+typedef union tuReg32 {
+    unsigned long Val32;
+    struct {
+    unsigned short LW;
+    unsigned short HW;
+    } Word;
+    char Val[4];
+} uReg32;
+
 int main(void) {
 	
     SetupClock();
     SwitchClocks();
     SetupPorts();
 
-    int i = 0;
-    for (i=0; i < 3; i++)
-    {
-        LED_1 = 1; LED_2 = 1; LED_3 = 1;
-        delay_ms(200);
-        LED_1 = 0; LED_2 = 0; LED_3 = 0;
-        delay_ms(200);
-        LED_1 = 1; LED_2 = 1; LED_3 = 1;
-        delay_ms(200);
+    // int i = 0;
+    // for (i=0; i < 3; i++)
+    // {
+    //     LED_1 = 1; LED_2 = 1; LED_3 = 1;
+    //     delay_ms(200);
+    //     LED_1 = 0; LED_2 = 0; LED_3 = 0;
+    //     delay_ms(200);
+    //     LED_1 = 1; LED_2 = 1; LED_3 = 1;
+    //     delay_ms(200);
+    // }
+
+    uReg32 addr;
+    addr.Val32 = 0x400;
+
+    char src_str[2];
+    src_str[1] = 0x00;
+    src_str[0] = 0x12;
+
+    pmWritePage(src_str,addr.Val32);
+
+    LED_1 = 1;
+    delay_ms(2000);
+
+    unsigned int res;
+
+    // unsigned long res = pmReadMem(addr.Val32);
+    // if (res == 0x0012){
+    //     LED_1=~LED_1;
+    //     delay_ms(2000);
+    // }
+
+    TBLPAG = 0x0;
+    res = __builtin_tblrdl(0x400);
+    if (res == 0x12){
+        LED_1 = ~LED_1;
+        delay_ms(1000);
     }
-    //Write Data to EEDATA space
-    TBLPAG = 0x7F;
-    __builtin_tblwth(0x00, DEFAULT_SRC_ADDR);
-    __builtin_tblwth(0x02, DEFAULT_DEST_ADDR);
-    __builtin_tblwth(0x04, DEFAULT_CHANNEL);
-    
+
+    // res = ReadLatch(0x0000, 0x0400);
+    // if (res == 0x12){
+    //      LED_1 = ~LED_1;
+    //      delay_ms(1000);
+    //  }
+
     radioSetup();   // polling mode
     bootSetup();
 
@@ -48,10 +85,12 @@ int main(void) {
 //
 //    // check if radio is working and at the right state.
     while (radioGetTrxState() != 0x16);
-//
-    LED_1 = 0;
-    LED_2 = 0;
-    LED_3 = 0;
+
+
+// //
+//     LED_1 = 0;
+//     LED_2 = 0;
+//     LED_3 = 0;
     
     bootTestRadio();
     bootRun();

a54fba84fae6c32ddd848ab5624560ddaf4d6529
ETumang <emily.tumang@gmail.com>
2014-06-17 17:24:53 -0400

    Removed some extra files.

--- imageproc/target/source/setadd.c
+++
@@ -1,9 +0,0 @@
-#include "radio_polling.h"
-#include "setadd.h"
-
-unsigned int addr = DEFAULT_SRC_ADDR;
-//static unsigned int GetAddr(void);
-
-unsigned int GetAddr(void) {
-    return addr;
-}
--- imageproc/target/source/setadd.h
+++
@@ -1,5 +0,0 @@
-#ifndef __SETADD_H
-#define __SETADD_H
-
-unsigned int GetAddr(void);
-#endif
